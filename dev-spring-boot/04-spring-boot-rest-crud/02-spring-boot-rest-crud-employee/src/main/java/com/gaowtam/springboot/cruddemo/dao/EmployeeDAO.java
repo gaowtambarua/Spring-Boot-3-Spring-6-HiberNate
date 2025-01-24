2abe99dcbd24f7c8ab4e3362db48b032df6613f1
Gaowtam Barua <128220812+gaowtambarua@users.noreply.github.com>
2025-01-24 16:36:15 +0600

    update

--- dev-spring-boot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/gaowtam/springboot/cruddemo/dao/EmployeeDAO.java
+++ dev-spring-boot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/gaowtam/springboot/cruddemo/dao/EmployeeDAO.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface EmployeeDAO {
     List<Employee> findALL();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
 }
--- dev-spring-boot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/gaowtam/springboot/cruddemo/rest/EmployeeRestControler.java
+++ dev-spring-boot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/gaowtam/springboot/cruddemo/rest/EmployeeRestControler.java
@@ -3,9 +3,7 @@ package com.gaowtam.springboot.cruddemo.rest;
 import com.gaowtam.springboot.cruddemo.dao.EmployeeDAO;
 import com.gaowtam.springboot.cruddemo.entity.Employee;
 import com.gaowtam.springboot.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.List;
@@ -41,9 +39,64 @@ public class EmployeeRestControler {
 //    }
 
 
-    @GetMapping("/employes")
+    @GetMapping("/employees")
     public List<Employee> fidAll()
     {
         return employeeService.findAll();
     }
+
+    //add mapping get /employess/{employeeId}
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId)
+    {
+        Employee theEmployee=employeeService.findById(employeeId);
+
+        if(theEmployee==null)
+        {
+            throw new RuntimeException("Emoployee id not found - "+employeeId);
+        }
+        return theEmployee;
+    }
+
+    // add mapping for POST / employees -add new employee
+    //save work
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee thEmployee)
+    {
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+
+        thEmployee.setId(0);
+        Employee dbEMEmployee=employeeService.save(thEmployee);
+        return dbEMEmployee;
+    }
+
+    // add mapping for PUT / employees - update existing employee
+    //update work
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee)
+    {
+        Employee dbEmployee=employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    // add mapping for delete /employees{employeeId} - delete employee
+    //delete work
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId)
+    {
+        Employee theEmployee=employeeService.findById(employeeId);
+
+        //throw exception if null
+        if(theEmployee==null)
+        {
+            throw new RuntimeException("Employee id not found - "+employeeId);
+        }
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - "+employeeId;
+    }
+
 }
