e112674de6d27f360ee5e5650ec7975bf315eb8c
Gaowtam Barua <128220812+gaowtambarua@users.noreply.github.com>
2025-01-29 02:54:56 +0600

    update

---
+++ dev-spring-boot/07-spring-boot-spring-mvc/01-thymleafdemo-helloworld/src/main/java/com/gaowtam/springboot/thymleafdemo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.gaowtam.springboot.thymleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+    // create a mapping for "/hello"
+
+    @GetMapping("/hello")
+    public String sayHello(Model thModel)
+    {
+        thModel.addAttribute("theDate",java.time.LocalDateTime.now());
+        return "helloworld";
+    }
+
+}
