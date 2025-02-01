01c07bf8b4c6bab871d73e6d2d74af6ddce55d23
Gaowtam Barua <128220812+gaowtambarua@users.noreply.github.com>
2025-02-02 02:39:31 +0600

    update

--- dev-spring-boot/07-spring-boot-spring-mvc/01-thymleafdemo-helloworld/src/main/java/com/gaowtam/springboot/thymleafdemo/controller/StudentController.java
+++ dev-spring-boot/07-spring-boot-spring-mvc/01-thymleafdemo-helloworld/src/main/java/com/gaowtam/springboot/thymleafdemo/controller/StudentController.java
@@ -1,14 +1,26 @@
 package com.gaowtam.springboot.thymleafdemo.controller;
 
 import com.gaowtam.springboot.thymleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${systems}")
+    private List<String> systems;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model thModel) {
@@ -16,6 +28,16 @@ public class StudentController {
         Student thStudent = new Student();
         // add student object to the model
         thModel.addAttribute("student", thStudent);
+
+        // add the list of countries to the model
+        thModel.addAttribute("countries",countries);
+
+        // add the list of languages th the model
+        thModel.addAttribute("languages",languages);
+
+        // add the list of systems to the model
+        thModel.addAttribute("systems",systems);
+
         return "student-form";
     }
 
