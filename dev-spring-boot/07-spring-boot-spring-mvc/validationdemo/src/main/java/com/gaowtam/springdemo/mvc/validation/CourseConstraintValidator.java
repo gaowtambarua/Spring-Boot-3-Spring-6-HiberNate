48e0aa6dd8791282eb3c2e77ca7bfc125ad7ba27
Gaowtam Barua <128220812+gaowtambarua@users.noreply.github.com>
2025-02-03 03:29:05 +0600

    update

--- dev-spring-boot/07-spring-boot-spring-mvc/validationdemo/src/main/java/com/gaowtam/springdemo/mvc/validation/CourseConstraintValidator.java
+++ dev-spring-boot/07-spring-boot-spring-mvc/validationdemo/src/main/java/com/gaowtam/springdemo/mvc/validation/CourseConstraintValidator.java
@@ -1,22 +1,31 @@
-//package com.gaowtam.springdemo.mvc.validation;
-//
-//import jakarta.validation.ConstraintValidator;
-//import jakarta.validation.ConstraintValidatorContext;
-//
-//public class CourseConstraintValidator implements ConstraintValidator<CourseCode,String> {
-//
-//    private String coursePrefix;
-//
-//    @Override
-//    public void initialize(CourseCode thCourseCode) {
-//        coursePrefix=thCourseCode.value(); //CourseCode class er moddhe value set kora ace
-//    }
-//
-//    @Override
-//    public boolean isValid(String theCOde, ConstraintValidatorContext theConstraintValidatorContext) {
-//
-//        boolean result=theCOde.startsWith(coursePrefix);
-//
-//        return result;
-//    }
-//}
+package com.gaowtam.springdemo.mvc.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseConstraintValidator implements ConstraintValidator<CourseCode,String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode thCourseCode) {
+        coursePrefix=thCourseCode.value(); //CourseCode class er moddhe value set kora ace
+    }
+
+    @Override
+    public boolean isValid(String theCOde, ConstraintValidatorContext theConstraintValidatorContext) {
+
+        boolean result;
+
+        if(theCOde!=null)
+        {
+            result=theCOde.startsWith(coursePrefix);
+        }
+        else
+        {
+            result=false;
+        }
+
+        return result;
+    }
+}
