49821132b297730765d315bbcc591a33fdce744a
Gaowtam Barua <128220812+gaowtambarua@users.noreply.github.com>
2025-02-04 02:52:37 +0600

    update

--- dev-spring-boot/08-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ dev-spring-boot/08-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -2,10 +2,10 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +31,48 @@ public class EmployeeController {
         // add to the spring model
         themodel.addAttribute("employees",theEmployees);
 
-        return  "list-employees";
+        return  "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel)
+    {
+        //create model add attribute to bind form data
+        Employee thEmployee=new Employee();
+        theModel.addAttribute("employee",thEmployee);
+        return "employees/employeee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee")Employee thEmployee)
+    {
+        // save the employee
+        employeeService.save(thEmployee);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";n
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId,Model thModel)
+    {
+        // get the employee form the service
+        Employee thEmployee=employeeService.findById(theId);
+
+        // set employee in the model to prepopulate the form
+        thModel.addAttribute("employee",thEmployee);
+
+        // sent over to our form
+
+        return "employees/employeee-form";
+    }
+
+    @GetMapping("/showFormForDelete")
+    public String showFormForDelete(@RequestParam("employeeId")int theId)
+    {
+        //delete the employee
+        employeeService.deleteById(theId);
+        //redirect to the /eployees/list
+        return "redirect:/employees/list";
     }
 }
